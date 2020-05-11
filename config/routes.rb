685152c5ea03ae01a42f520e53474e4c6b20f631
James Benjamin <james.abenj@gmail.com>
2020-05-11 22:32:16 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'about', to: 'static#about'
+  get 'hello_world', to: 'static#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
